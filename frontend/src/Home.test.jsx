edbf2ff66c87324e6d9d5c8ecb9c58d3e7a9e1b3
Mayuri <mayuripaliwal8@gmail.com>
2026-08-08 20:20:39 +0530

    Add frontend testing using vitest

---
+++ frontend/src/Home.test.jsx
@@ -0,0 +1,15 @@
+import {expect,test} from "vitest";
+import { render,screen } from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+
+//tests whether the text "URL Shortener" appears on the Home Page
+test("Home page renders",()=>{
+    render(
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    );
+    expect(screen.getByText("URL Shortener")).toBeInTheDocument();
+});
+
