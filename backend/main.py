b5fc429f89f83fc77331f073ef038b22f318e027
Mayuri <mayuripaliwal8@gmail.com>
2026-08-14 23:51:24 +0530

    feat: Add users table for authentication
    Add a PostgreSQL users table with fields for user ID, email, username,
    and password hash

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,13 @@ cursor.execute("CREATE TABLE IF NOT EXISTS urls(" \
 "click_count INTEGER NOT NULL DEFAULT 0, " \
 "created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), " \
 "last_clicked_at TIMESTAMPTZ)")
+
+cursor.execute("CREATE TABLE IF NOT EXISTS users(" \
+"id SERIAL PRIMARY KEY, " \
+"email TEXT UNIQUE NOT NULL, " \
+"user_name TEXT NOT NULL, " \
+"password_hash TEXT NOT NULL )")
+
 conn.commit()
 
 
